66ce8a45d5433449b68bb58e41761aace872cc4c
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-06-27 23:26:46 -0300

    added flask bootstrap

--- hello.py
+++ hello.py
@@ -4,8 +4,10 @@ Intializing application
 Instance of flask framework
 """
 from flask import Flask, request, make_response, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/cookie')

8992a30c7901a6b8e46e9dd39ecb4b973d9ea32c
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-06-27 23:26:05 -0300

    template hot fix

--- hello.py
+++ hello.py
@@ -3,17 +3,20 @@ Intializing application
 
 Instance of flask framework
 """
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/cookie')
 def cookie():
-    """ Example of cookie usage """
+    """
+    Example of cookie usage
+    @return render
+    """
     response = make_response('<h1>This document carries a cookie!')
     response.set_cookie('answer', '42')
-    return response
+    return render_template('cookie.html', response=response)
 
 
 @app.route('/404')
@@ -26,10 +29,10 @@ def _404():
 def index():
     """ Home page """
     user_agent = request.headers.get('User-Agent')
-    return '<h1>Hello World, your browser is {}</h1>'.format(user_agent)
+    return render_template('index.html', response=user_agent)
 
 
 @app.route('/user/<name>')
 def user(name):
     """ Greetings user page """
-    return '<h1>Hello, {}</h1>'.format(name)
+    return render_template('user.html', name=name)

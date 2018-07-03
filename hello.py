1bb61f607ad01647490a5d3bd2bfebd78bd485af
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-07-03 01:17:52 -0300

    Installed SQLAlchemy Define the first model

--- hello.py
+++ hello.py
@@ -3,18 +3,53 @@ Intializing application
 
 Instance of flask framework
 """
-from flask import Flask, make_response, render_template, redirect, session, url_for, flash
+
+# chapter 5 - DATABASES
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, make_response,
+render_template, redirect, session, url_for, flash
+
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+
+# get absolute path of project directory
+basedir = os.path.asbpath(os.path.dirname(__file__))
+
 from wtforms.validators import DataRequired
 
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\ 'sqllite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 app.config['SECRET_KEY'] = 'My SECRET_KEY'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+# define models
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(dbs.Model):
+    """(User description)"""
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
@@ -61,7 +96,9 @@ def index():
             flash('Looks like you have changed your name, don\'t you?')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', current_time = datetime.utcnow(), name = session.get('name'), form = form)
+    return render_template('index.html',
+                           current_time=datetime.utcnow(),
+                           name=session.get('name'), form=form)
 
 
 @app.route('/user/<name>')

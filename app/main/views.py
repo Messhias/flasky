45c040f5b0a78316d3bf067e60e488a0c6b7659e
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-07-05 02:27:52 -0300

    created views file

---
+++ app/main/views.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from flask import render_template, session, reidrec, url_for
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+
+@main.route('/', methods=['POST', 'GET'])
+def index():
+    """ Home page """
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            # if app.config['FLASKY_ADMIN']:
+            #     send_email(app.config['FLASKY_ADMIN'],
+            #                'New user',
+            #                'user',
+            #                user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form,
+                           name=session.get('name'),
+                           known=session.get('known'))

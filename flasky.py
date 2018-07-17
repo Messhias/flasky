b48a742fb71c0ac774003460da76d1bb0fa30cb1
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-07-16 23:35:20 -0300

    updates

--- flasky.py
+++ flasky.py
@@ -1,7 +1,7 @@
 import os
+from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role
-from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -14,7 +14,7 @@ def make_shell_context():
 
 @app.cli.command()
 def test():
-    """Run the unit tests"""
+    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)

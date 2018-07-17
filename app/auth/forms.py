424ca65c900844e197ec2b7b901bc1c3528accfb
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-07-17 00:23:20 -0300

    Created login form py file

---
+++ app/auth/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
+
+
+class LoginForm(FlaskForm):
+    """docstring for LoginForm."""
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Length(1, 64),
+        Email()
+    ])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Keep me logged in')
+    submit = SubmitField('Log In')
